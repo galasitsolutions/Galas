dca4075f6241760beecc44dd8b2849fab377f8c9
galasitsolutions <info@galasitsolutions.com>
2024-08-18 12:38:25 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,43 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import compression from "vite-plugin-compression";
-import { createHtmlPlugin } from "vite-plugin-html";
-import path from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-import bundleAnalyzer from "vite-plugin-bundle-analyzer";
-const { ViteBundlerAnalyzer } = bundleAnalyzer;
-
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    compression({
-      algorithm: "brotliCompress",
-      threshold: 10240,
-    }),
-    ViteBundlerAnalyzer(),
-    createHtmlPlugin({
-      minify: true,
-    }),
-  ],
-  build: {
-    outDir: "dist",
-    minify: "terser",
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
-      mangle: true,
-    },
-    css: {
-      postcss: {
-        plugins: [require("tailwindcss"), require("autoprefixer")],
-      },
-    },
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve("./src"),
-    },
-  },
-});
+  plugins: [react()],
+})
