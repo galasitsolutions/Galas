75275d28daf9005c9b69368196b44f262a903758
gajndranath <gajendra.tripathi.me@gmail.com>
2024-08-30 14:35:45 +0530

    fixed changes

--- vite.config.js
+++ vite.config.js
@@ -1,41 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import compression from "vite-plugin-compression";
-import { ViteBundlerAnalyzer } from "vite-plugin-bundle-analyzer";
-import { createHtmlPlugin } from "vite-plugin-html";
-import path from "path";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    compression({
-      algorithm: "brotliCompress",
-      threshold: 10240,
-    }),
-    ViteBundlerAnalyzer(),
-    createHtmlPlugin({
-      minify: true,
-    }),
-  ],
-  build: {
-    outDir: "dist",
-    minify: "terser",
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
-      mangle: true,
-    },
-    css: {
-      postcss: {
-        plugins: [require("tailwindcss"), require("autoprefixer")],
-      },
-    },
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve("./src"), // Updated to be compatible with ES module
-    },
-  },
+  plugins: [react()],
 });
