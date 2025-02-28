757d93250ca9762b939618184a3edceab7c57f42
Lovely Tiwari <lovelyt7989@gmail.com>
2025-02-28 12:14:44 +0530

    for making pull

--- src/components/section/HeroSection.jsx
+++ src/components/section/HeroSection.jsx
@@ -1,12 +1,56 @@
 import { Suspense, lazy } from "react";
 import useWindowSize from "../../hooks/useWindowSize";
 import HeroBanner from "../../assets/img/product/hero-banner.webp";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import arrow2 from "../../assets/img/icons/arrow2.svg";
 
 // Lazy load the CircleText component
 const CircleText = lazy(() => import("../ui/CircleText"));
 
 export default function HeroSection() {
   const { width } = useWindowSize();
+
+  const settings = {
+    dots: false,
+    arrows: false,
+    infinite: true,
+    speed: 5000,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 0,
+    cssEase: "linear", 
+    pauseOnHover: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 320,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
+  
 
   return (
     <>
@@ -60,6 +104,46 @@ export default function HeroSection() {
           </div>
         </div>
       </section>
+      <div className="grid grid-cols-1 bg-blue-600 relative flex items-center mx-auto">
+  <Slider {...settings}>
+    <div className="flex flex-col items-center p-0">
+      <h1 className="text-4xl font-bold flex items-center justify-evenly">
+        <span className=" tracking-wider">WEB</span>
+        <span className=" tracking-wider">DEVELOPMENT</span>
+        <span className="">
+          <img src={arrow2} alt="arrow" className="w-8  filter invert m-0 p-0"  />
+        </span>
+      </h1>
+    </div>
+    <div className="flex flex-col items-center p-0">
+    <h1 className="text-4xl  font-bold flex items-center justify-evenly ">
+      <span className=" tracking-wider">IT</span>
+      <span className=" tracking-wider">PRODUCTS</span>
+      <span className="">
+        <img src={arrow2} alt="arrow" className="w-8  filter invert m-0 p-0"  />
+      </span>
+    </h1>
+    </div>
+    <div className="flex flex-col items-center  p-0">
+    <h1 className="text-4xl font-bold flex items-center justify-evenly">
+      <span className=" tracking-wider">DIGITAL</span>
+      <span className=" tracking-wider">MARKETING</span>
+      <span className="">
+        <img src={arrow2} alt="arrow" className="w-8  filter invert" />
+      </span>
+    </h1>
+    </div>
+  </Slider>
+</div>
+
+  {/* <div className="absolute top-0 py-4 bg-blue-600 animate-marquee2 whitespace-nowrap">
+    <span class="text-4xl mx-4">Marquee Item 1</span>
+    <span class="text-4xl mx-4">Marquee Item 2</span>
+    <span class="text-4xl mx-4">Marquee Item 3</span>
+    <span class="text-4xl mx-4">Marquee Item 4</span>
+    <span class="text-4xl mx-4">Marquee Item 5</span>
+  </div> */}
+
     </>
   );
 }
